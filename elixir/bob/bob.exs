f1d67f0993e40b88a91e5e7d211cd464364ccfc3
Marcos Brizeno <marcos.uece.comp@gmail.com>
2015-10-06 21:33:43 -0300

    Extract conditionals to methods

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,11 +1,27 @@
 defmodule Teenager do
   def hey(input) do
     cond do
-      String.strip(input) == "" -> "Fine. Be that way!"
-      String.ends_with?(input, "?") -> "Sure."
-      String.match?(input, ~r/^(\d|,|\s)+$/) -> "Whatever."
-      input == String.upcase(input) -> "Whoa, chill out!"
+      ignored?(input) -> "Fine. Be that way!"
+      questioned?(input) -> "Sure."
+      nonsense?(input) -> "Whatever."
+      yelled?(input) -> "Whoa, chill out!"
       true -> "Whatever."
     end
+  end
+
+  defp ignored?(input) do
+    String.strip(input) == ""
+  end
+
+  defp questioned?(input) do
+    String.ends_with?(input, "?")
+  end
+
+  defp nonsense?(input) do
+    String.match?(input, ~r/^(\d|,|\s)+$/)
+  end
+
+  defp yelled?(input) do
+    input == String.upcase(input)
   end
 end
