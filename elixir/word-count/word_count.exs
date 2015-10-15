41d5fcf8fe1313ed5c8db083bee46d1c481688fa
Marcos Brizeno <marcos.uece.comp@gmail.com>
2015-10-15 07:29:18 -0300

    [elixir] First iteration for word count

--- elixir/word-count/word_count.exs
+++ elixir/word-count/word_count.exs
@@ -6,6 +6,20 @@ defmodule Words do
   """
   @spec count(String.t) :: map()
   def count(sentence) do
+    String.split(sentence, ~r/(\s|_)/)
+      |> group_elements
+  end
 
+  defp group_elements(elements) do
+    Enum.reject(elements, fn(e) -> !String.match?(e, ~r/\w+/) end)
+      |> Enum.map(fn(e) ->
+        String.downcase(e)
+          |> String.split("")
+          |> Enum.reject(fn(c) -> !String.match?(c, ~r/(\w|-)/) end)
+          |> Enum.join
+        end)
+      |> Enum.reduce(%{}, fn(element, acc) ->
+        Map.put(acc, element, (acc[element] || 0) + 1)
+      end)
   end
 end
