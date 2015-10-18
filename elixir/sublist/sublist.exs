2f1e3f49fff1d342c15f8beeddb55a9f6bf8e09c
Marcos Brizeno <marcos.uece.comp@gmail.com>
2015-10-18 08:02:33 -0200

    [elixir] Return :equal if lists are equal

--- elixir/sublist/sublist.exs
+++ elixir/sublist/sublist.exs
@@ -3,7 +3,7 @@ defmodule Sublist do
   Returns whether the first list is a sublist or a superlist of the second list
   and if not whether it is equal or unequal to the second list.
   """
-  def compare(a, b) do
-
+  def compare(a, b) when a == b do
+    :equal
   end
 end
