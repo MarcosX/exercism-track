82e9e4f13494aa960d2a7797f434afe4eafcdda9
Marcos Brizeno <marcos.uece.comp@gmail.com>
2015-12-31 18:06:21 -0200

    [elixir] rna transcription using reduce

---
+++ elixir/rna-transcription/dna.exs
@@ -0,0 +1,15 @@
+defmodule DNA do
+  @nucleotides %{?G => 'C',?C => 'G',?T => 'A',?A => 'U'}
+  @doc """
+  Transcribes a character list representing DNA nucleotides to RNA
+
+  ## Examples
+
+  iex> DNA.to_rna('ACTG')
+  'UGAC'
+  """
+  @spec to_rna([char]) :: [char]
+  def to_rna(dna) do
+    Enum.reduce(dna, '', &(Enum.concat(&2,@nucleotides[&1])))
+  end
+end
