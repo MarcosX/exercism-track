2173910878a65be6094ac3059649884ec5574095
Marcos Brizeno <marcos.uece.comp@gmail.com>
2016-02-16 10:48:47 -0200

    [javascript] Update hamming exercise

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -1,12 +1,14 @@
-compute = function(a_dna, b_dna) {
+var Hamming = function() {};
+
+Hamming.prototype.compute = function(a_dna, b_dna) {
   if(a_dna.length != b_dna.length) {
     throw new Error('DNA strands must be of equal length.');
   }
   
-  return calculate_difference(a_dna, b_dna);
+  return this.calculate_difference(a_dna, b_dna);
 }
 
-calculate_difference = function(a_dna, b_dna) {
+Hamming.prototype.calculate_difference = function(a_dna, b_dna) {
   var count = 0;
   for(var i = 0; i < a_dna.length; i++) {
     if(a_dna[i] != b_dna[i]) {
@@ -17,4 +19,4 @@ calculate_difference = function(a_dna, b_dna) {
   return count;
 }
 
-module.exports = compute;
+module.exports = Hamming;
