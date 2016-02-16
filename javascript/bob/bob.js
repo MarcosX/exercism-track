2a0fce78df2a212ca39e0df034c6d0f6e7de0c38
Marcos Brizeno <marcos.uece.comp@gmail.com>
2016-02-16 11:17:23 -0200

    [javascript] Split into smallers functions

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -3,38 +3,46 @@
 // convenience to get you started writing code faster.
 //
 
-var Bob = function() {};
-
-Bob.prototype.hey = function(input) {
-         var WHATEVER = 'Whatever.',
-         CHILL = 'Whoa, chill out!',
-         SURE = 'Sure.',
-         FINE = 'Fine. Be that way!';
-
-         var chillRe = /^[A-Z|\W]+\!$/,
-             forcefulQuestionRe = /^[A-Z|\W]+\?$/,
-             shoutingRe = /^[A-Z]+$/,
-             questionRe = /\?$/,
-             nonWordsRe = /[\d|\s]+/g;
-
-         var onlyLetters = input.replace(nonWordsRe, '');
-
-         if (chillRe.test(onlyLetters)) {
-           return CHILL;
-         }
-         if (forcefulQuestionRe.test(onlyLetters)) {
-           return CHILL;
-         }
-         if (shoutingRe.test(onlyLetters)) {
-           return CHILL;
-         }
-         if (questionRe.test(input)) {
-           return SURE;
-         }
-         if (onlyLetters === '') {
-           return FINE;
-         }
-         return WHATEVER;
+var Bob = function() {
+  this.WHATEVER = 'Whatever.';
+  this.CHILL = 'Whoa, chill out!';
+  this.SURE = 'Sure.';
+  this.FINE = 'Fine. Be that way!';
 };
+
+Bob.prototype = {
+  hey: function(input) {
+         var onlyValidChars = this.filterInvalidChars(input);
+
+         if (this.yelling(onlyValidChars)) {
+           return this.CHILL;
+         } else if (this.questioning(onlyValidChars)) {
+           return this.SURE;
+         } else if (this.silecing(onlyValidChars)) {
+           return this.FINE;
+         }
+
+         return this.WHATEVER;
+  },
+
+  yelling: function(input) {
+             var forcefuQuestionExclamationRe = /^[A-Z|\W]+[\!|\?]$/,
+             shoutingRe = /^[A-Z]+$/;
+
+             return forcefuQuestionExclamationRe.test(input) || shoutingRe.test(input);
+           },
+
+  questioning: function(input) {
+                 return /\?$/.test(input);
+               },
+
+  silecing: function(input) {
+              return input === '';
+            },
+
+  filterInvalidChars: function(input) {
+                        return input.replace(/[\d|\s]+/g, '');
+                      }
+}
 
 module.exports = Bob;
