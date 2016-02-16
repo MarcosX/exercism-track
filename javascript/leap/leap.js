e493f5cbc013e3c46723f0417f38c65016de0d2e
Marcos Brizeno <marcos.uece.comp@gmail.com>
2016-02-16 10:44:44 -0200

    [javascript] Update leap exercise

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,9 +1,13 @@
-var isLeapYear = function(year){
-  var divisibleBy4   = year%4 == 0,
-      divisibleBy100 = year%100 == 0,
-      divisibleBy400 = year%400 == 0;
+var Year = function(year) {
+  this.year = year;
+};
+
+Year.prototype.isLeap = function(){
+  var divisibleBy4   = this.year%4 == 0,
+      divisibleBy100 = this.year%100 == 0,
+      divisibleBy400 = this.year%400 == 0;
 
   return divisibleBy4 && !divisibleBy100 || divisibleBy400;
 }
 
-module.exports = isLeapYear;
+module.exports = Year;
