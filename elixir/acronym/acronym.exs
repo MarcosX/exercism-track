a0d1643f87b69d252694dfebd82802af7112d86b
Marcos Brizeno <marcos.uece.comp@gmail.com>
2016-03-06 14:11:14 -0300

    [elixir] First try on acronym

---
+++ elixir/acronym/acronym.exs
@@ -0,0 +1,16 @@
+defmodule Acronym do
+  @doc """
+  Generate an acronym from a string.
+  "This is a string" => "TIAS"
+  """
+  @spec abbreviate(string) :: String.t()
+  def abbreviate(string) do
+    String.split(string, ~r{(\s|-|[a-z](?=[A-Z]))})
+    |> Enum.map(&(captilize_first_letter(&1)))
+    |> Enum.join
+  end
+
+  defp captilize_first_letter(string) do
+    String.capitalize(String.first(string))
+  end
+end
