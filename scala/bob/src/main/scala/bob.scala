9b3b23a898e8dfb24e90a61763f33409829dafde
Marcos Brizeno <marcos.uece.comp@gmail.com>
2016-05-23 07:07:40 -0300

    [scala] Refactoring Bob to use case

--- scala/bob/src/main/scala/bob.scala
+++ scala/bob/src/main/scala/bob.scala
@@ -2,14 +2,26 @@ package scala;
 
 class Bob {
   def hey(message:String):String = {
+    replyTo(findTypeOf(message))
+  }
+
+  private def replyTo(messageType:String):String = messageType match {
+    case "shouting" => "Whoa, chill out!"
+    case "questioning" => "Sure."
+    case "silencing" => "Fine. Be that way!"
+    case _ => "Whatever."
+  }
+
+  private def findTypeOf(message:String):String = {
     if(shouting(message)) {
-      return "Whoa, chill out!"
+      "shouting"
     } else if(questioning(message)) {
-      return "Sure."
+      "questioning"
     } else if(silencing(message)) {
-      return "Fine. Be that way!"
+      "silencing"
+    } else {
+      "none"
     }
-    "Whatever."
   }
 
   private def questioning(message:String):Boolean = {
