9dccae8cfd6676f378e5056f9fe6c51a11ea9a2e
Marcos Brizeno <marcos.uece.comp@gmail.com>
2016-08-08 12:03:59 -0400

    [elixir] Refactor to remove function and rename vars

--- elixir/space-age/space_age.exs
+++ elixir/space-age/space_age.exs
@@ -1,13 +1,15 @@
 defmodule SpaceAge do
-  @earth_years_to %{
+  @earth_years_ratio_to %{
     :mercury => 0.2408467,
     :venus => 0.61519726,
+    :earth => 1,
     :mars => 1.8808158,
     :jupiter => 11.862615,
     :saturn => 29.447498,
     :uranus => 84.016846,
     :neptune => 164.79132
   }
+  @seconds_in_earth_year 31_557_600
   @type planet :: :mercury | :venus | :earth | :mars | :jupiter
                 | :saturn | :neptune | :uranus
 
@@ -16,11 +18,7 @@ defmodule SpaceAge do
   aged on 'planet'.
   """
   @spec age_on(planet, pos_integer) :: float
-  def age_on(:earth, seconds) do
-    seconds/31_557_600
-  end
-
   def age_on(planet, seconds) do
-    age_on(:earth, seconds)/@earth_years_to[planet]
+    (seconds/@seconds_in_earth_year)/@earth_years_ratio_to[planet]
   end
 end
