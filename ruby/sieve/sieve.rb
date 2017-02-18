4308897f96990ea8f22ae7acb85ccf6f32e89f6b
Marcos Brizeno <marcos.uece.comp@gmail.com>
2017-02-18 17:47:35 -0200

    [ruby] Initial solution using sieve algorithm

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -4,6 +4,14 @@ class Sieve
   end
 
   def primes
-    []
+    primes = (2..@limit).to_a
+    primes.each do |number, is_prime|
+        multiplier = 2
+        until number * multiplier > @limit do
+          primes.delete(number*multiplier)
+          multiplier += 1
+        end
+    end
+    primes
   end
 end
