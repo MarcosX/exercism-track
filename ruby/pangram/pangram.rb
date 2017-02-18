1e79f53ef5c64f61e3d206e8559530e2c3b515a6
Marcos Brizeno <marcos.uece.comp@gmail.com>
2017-02-18 10:57:05 -0200

    [ruby] Resolve pangram exercise

---
+++ ruby/pangram/pangram.rb
@@ -0,0 +1,11 @@
+class Pangram
+  TOTAL_AMOUNT_OF_LETTERS = 26
+  def self.pangram?(phrase)
+    count_unique_letters(phrase) == TOTAL_AMOUNT_OF_LETTERS
+  end
+
+  private
+  def self.count_unique_letters(phrase)
+    phrase.downcase.split('').select {|char| char =~ /[a-z]/}.uniq.size
+  end
+end
