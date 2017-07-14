c3a7f4aafca8bcbcf1ffefdc1a87296f1e1f26e0
Marcos Brizeno <marcos.uece.comp@gmail.com>
2017-07-14 18:01:35 -0300

    [clojure] Extract functions for better readability

--- clojure/bob/src/bob.clj
+++ clojure/bob/src/bob.clj
@@ -1,15 +1,35 @@
 (ns bob)
 
+(defn nothing? [phrase]
+  (= (clojure.string/trim phrase) "")
+)
+
+(defn nonsense? [phrase]
+  (re-matches #"^(\d|,|\s)+$" phrase)
+)
+
+(defn forceful? [phrase]
+  (= phrase (clojure.string/upper-case phrase))
+)
+
+(defn forceful-question? [phrase]
+  (re-matches #"^(\d.*)\?$" phrase)
+)
+
+(defn question? [phrase]
+  (clojure.string/ends-with? phrase "?")
+)
+
 (defn response-for [phrase]
-  (if (= (clojure.string/trim phrase) "")
+  (if (nothing? phrase)
     "Fine. Be that way!"
-    (if (re-matches #"^(\d|,|\s)+$" phrase)
+    (if (nonsense? phrase)
       "Whatever."
-    (if (= phrase (clojure.string/upper-case phrase))
-      (if (re-matches #"^(\d.*)\?$" phrase)
+    (if (forceful? phrase)
+      (if (forceful-question? phrase)
         "Sure."
         "Whoa, chill out!")
-      (if (clojure.string/ends-with? phrase "?")
+      (if (question? phrase)
         "Sure."
         "Whatever."))))
 )
