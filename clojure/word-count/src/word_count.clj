c72685939ab724589548167c0cc45bb387b27495
Marcos Brizeno <marcos.uece.comp@gmail.com>
2017-07-16 07:45:32 -0300

    [clojure] Hide functions and use assoc to increment hash

--- clojure/word-count/src/word_count.clj
+++ clojure/word-count/src/word_count.clj
@@ -1,20 +1,20 @@
 (ns word-count)
 
-(defn word-count-reducer [word-counter word]
+(defn- word-count-reducer [word-counter word]
   (if (contains? word-counter word)
-    (into word-counter {word (inc (get word-counter word))})
+    (assoc word-counter word (inc (get word-counter word)))
     (into word-counter {word 1})))
 
-(defn lower-case-words [word]
+(defn- lower-case-words [word]
   (clojure.string/lower-case word))
 
-(defn clear-non-letters [phrase]
+(defn- clear-non-letters [phrase]
   (clojure.string/replace phrase #"[^a-z|A-Z|\s|0-9]" ""))
 
-(defn split-words [phrase]
+(defn- split-words [phrase]
   (clojure.string/split phrase #"\s+"))
 
-(defn break-words [phrase]
+(defn- break-words [phrase]
   (map lower-case-words (split-words (clear-non-letters phrase))))
 
 (defn word-count [phrase]
