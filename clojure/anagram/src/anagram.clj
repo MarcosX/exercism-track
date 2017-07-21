516fe1d1b35edafffcf848430b15574be64eb5be
Marcos Brizeno <marcos.uece.comp@gmail.com>
2017-07-21 16:43:15 -0300

    [clojure] Move same-word check to filter

--- clojure/anagram/src/anagram.clj
+++ clojure/anagram/src/anagram.clj
@@ -9,10 +9,8 @@
   (= (clojure.string/lower-case base-word) (clojure.string/lower-case word-to-compare)))
 
 (defn- is-anagram? [base-word word-to-compare]
-  (if (is-same-word? base-word word-to-compare)
-    false
-    (= (sorted-chars base-word) (sorted-chars word-to-compare))))
+    (= (sorted-chars base-word) (sorted-chars word-to-compare)))
 
 (defn anagrams-for [word anagrams]
-  (filter #(is-anagram? % word) anagrams)
+  (filter #(and (not (is-same-word? % word)) (is-anagram? % word)) anagrams)
 )
