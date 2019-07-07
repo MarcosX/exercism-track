212b8f27382bbc8e50e87c859afd0344cfaf6742
MarcosX <marcos.uece.comp@gmail.com>
2019-07-07 16:40:00 -0500

    [elixir] create functions to validate robot and simplify guard clauses

--- elixir/robot-simulator/robot_simulator.exs
+++ elixir/robot-simulator/robot_simulator.exs
@@ -1,7 +1,7 @@
 defmodule RobotSimulator do
 
   defmodule Robot do
-    defstruct position: {0, 0}, direction: :north
+    defstruct [:position, :direction]
   end
   
   @doc """
@@ -10,20 +10,25 @@ defmodule RobotSimulator do
   Valid directions are: `:north`, `:east`, `:south`, `:west`
   """
   @spec create(direction :: atom, position :: {integer, integer}) :: %Robot{}
-  def create(direction, _position) when direction not in [:north, :south, :east, :west] do
+  def create(direction \\ :north, position \\ {0, 0})  do
+    %Robot{direction: direction, position: position}
+    |> validate_robot
+  end
+
+  defp validate_robot(%{direction: direction, position: _position}) when direction not in [:north, :south, :east, :west] do
     {:error, "invalid direction"}
   end
 
-  def create(_direction, position) when not is_tuple(position) or tuple_size(position) != 2 do
+  defp validate_robot(%{direction: _direction, position: position}) when not is_tuple(position) or tuple_size(position) != 2 do
     {:error, "invalid position"}
   end
 
-  def create(_direction, {x, y}) when not is_number(x) or not is_number(y) do
+  defp validate_robot(%{direction: _direction, position: {x, y}}) when not is_number(x) or not is_number(y) do
     {:error, "invalid position"}
   end
 
-  def create(direction \\ :north, position \\ {0, 0})  do
-    %Robot{direction: direction, position: position}
+  defp validate_robot(robot) do
+    robot
   end
 
   @doc """
@@ -34,8 +39,7 @@ defmodule RobotSimulator do
   @spec simulate(robot :: any, instructions :: String.t()) :: any
   def simulate(robot, instructions) do
     instructions
-    |> String.split("")
-    |> Enum.reject(&(&1 == ""))
+    |> String.split("", trim: true)
     |> simulate_instructions(robot)
   end
 
@@ -43,7 +47,11 @@ defmodule RobotSimulator do
     robot
   end
 
-  defp simulate_instructions([instruction | instructions], robot) when instruction == "R" do
+  defp simulate_instructions([instruction | _instructions], _robot) when instruction not in ["L", "R", "A"] do
+    {:error, "invalid instruction"}
+  end
+
+  defp simulate_instructions(["R" | instructions], robot) do
     new_robot = case robot.direction do
       :north -> %Robot{position: robot.position, direction: :east}
       :east -> %Robot{position: robot.position, direction: :south}
@@ -53,7 +61,7 @@ defmodule RobotSimulator do
     simulate_instructions(instructions, new_robot)
   end
 
-  defp simulate_instructions([instruction | instructions], robot) when instruction == "L" do
+  defp simulate_instructions(["L" | instructions], robot) do
     new_robot = case robot.direction do
       :north -> %Robot{position: robot.position, direction: :west}
       :west -> %Robot{position: robot.position, direction: :south}
@@ -63,20 +71,14 @@ defmodule RobotSimulator do
     simulate_instructions(instructions, new_robot)
   end
 
-  defp simulate_instructions([instruction | instructions], robot) when instruction == "A" do
-    {x, y} = robot.position
-    direction = robot.direction
-    new_robot = case robot.direction do
+  defp simulate_instructions(["A" | instructions], %{direction: direction, position: {x, y}} = _robot) do
+    new_robot = case direction do
       :north -> %Robot{position: {x, y + 1}, direction: direction}
       :south -> %Robot{position: {x, y - 1}, direction: direction}
       :east -> %Robot{position: {x + 1, y}, direction: direction}
       :west -> %Robot{position: {x - 1, y}, direction: direction}
     end
     simulate_instructions(instructions, new_robot)
-  end
-
-  defp simulate_instructions(_instructions, _robot) do
-    {:error, "invalid instruction"}
   end
 
   @doc """
