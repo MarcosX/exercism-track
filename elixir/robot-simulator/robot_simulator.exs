f7737a159ca8898ea576da6b7f0e5312c4ddbd80
MarcosX <marcos.uece.comp@gmail.com>
2019-07-10 22:32:58 -0500

    [elixir] simplify function def

--- elixir/robot-simulator/robot_simulator.exs
+++ elixir/robot-simulator/robot_simulator.exs
@@ -30,35 +30,28 @@ defmodule RobotSimulator do
     end
   end
 
+  defguard integer_position(x, y) when is_integer(x) and is_integer(y)
+  defguard valid_position(position) when is_tuple(position) and tuple_size(position) == 2
+  defguard valid_direction(direction) when direction in [:north, :south, :east, :west]
+
   @doc """
   Create a Robot Simulator given an initial direction and position.
 
   Valid directions are: `:north`, `:east`, `:south`, `:west`
   """
   @spec create(direction :: atom, position :: {integer, integer}) :: %Robot{}
-  def create(direction \\ :north, position \\ {0, 0}) do
-    %Robot{direction: direction, position: position}
-    |> validate_robot
-  end
+  def create(direction \\ :north, position \\ {0, 0}),
+    do: %Robot{direction: direction, position: position} |> validate_robot
+
+  defp validate_robot(%{direction: direction, position: {x, y} = position} = robot)
+       when integer_position(x, y) and valid_position(position) and valid_direction(direction),
+       do: robot
 
   defp validate_robot(%{direction: direction, position: _position})
-       when direction not in [:north, :south, :east, :west] do
-    {:error, "invalid direction"}
-  end
+       when not valid_direction(direction),
+       do: {:error, "invalid direction"}
 
-  defp validate_robot(%{direction: _direction, position: position})
-       when not is_tuple(position) or tuple_size(position) != 2 do
-    {:error, "invalid position"}
-  end
-
-  defp validate_robot(%{direction: _direction, position: {x, y}})
-       when not is_number(x) or not is_number(y) do
-    {:error, "invalid position"}
-  end
-
-  defp validate_robot(robot) do
-    robot
-  end
+  defp validate_robot(_robot), do: {:error, "invalid position"}
 
   @doc """
   Simulate the robot's movement given a string of instructions.
@@ -76,15 +69,13 @@ defmodule RobotSimulator do
     end
   end
 
-  defp follow_instructions(instruction, _robot) when instruction not in ["L", "R", "A"] do
-    throw({:error, "invalid instruction"})
-  end
-
   defp follow_instructions("R", robot), do: Robot.turn_right(robot)
 
   defp follow_instructions("L", robot), do: Robot.turn_left(robot)
 
   defp follow_instructions("A", robot), do: Robot.advance(robot)
+
+  defp follow_instructions(_instruction, _robot), do: throw({:error, "invalid instruction"})
 
   @doc """
   Return the robot's direction.
@@ -92,11 +83,11 @@ defmodule RobotSimulator do
   Valid directions are: `:north`, `:east`, `:south`, `:west`
   """
   @spec direction(robot :: any) :: atom
-  def direction(%{ position: _position, direction: direction } = _robot), do: direction
+  def direction(%{position: _position, direction: direction}), do: direction
 
   @doc """
   Return the robot's position.
   """
   @spec position(robot :: any) :: {integer, integer}
-  def position(%{ position: position, direction: _direction } = _robot), do: position
+  def position(%{position: position, direction: _direction}), do: position
 end
