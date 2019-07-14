2caf1e789bbb4f1f1a0ba48a780c514d8d2e5888
MarcosX <marcos.uece.comp@gmail.com>
2019-07-14 09:31:27 -0500

    [python] use conditional to build list with mismatching dna

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -7,4 +7,4 @@ def distance(strand_a, strand_b):
     """
     if len(strand_a) != len(strand_b):
         raise ValueError("Mismatch in strands length")
-    return sum(sa != sb for sa, sb in zip(strand_a, strand_b))
+    return sum(1 for dna_a, dna_b in zip(strand_a, strand_b) if dna_a != dna_b)
