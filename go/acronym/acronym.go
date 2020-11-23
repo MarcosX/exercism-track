362bdb871fa866e5bdb6fc6666298fe1066b10e0
Marcos Brizeno <marcos.uece.comp@gmail.com>
2020-11-23 15:12:24 -0600

    [Go] Solution to acronym

---
+++ go/acronym/acronym.go
@@ -0,0 +1,24 @@
+// This is a "stub" file.  It's a little start on your solution.
+// It's not a complete solution though; you have to write some code.
+
+// Package acronym should have a package comment that summarizes what it's about.
+// https://golang.org/doc/effective_go.html#commentary
+package acronym
+
+import (
+  "strings"
+  "regexp"
+)
+
+// Abbreviate should have a comment documenting it.
+func Abbreviate(s string) string {
+	abbreviation := ""
+  formattedString := strings.ToUpper(strings.ReplaceAll(s, "'", ""))
+  re := regexp.MustCompile("[a-zA-Z]+")
+  wordsRange := re.FindAllIndex([]byte(formattedString), -1)
+  for _, wordRange := range wordsRange {
+    firstLetter := string(formattedString[wordRange[0]])
+    abbreviation = abbreviation + firstLetter
+  }
+	return abbreviation
+}
