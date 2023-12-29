ac64cf715417fa3026dbeef760288897fdd4ddb5
Marcos Brizeno <marcos.uece.comp@gmail.com>
2023-12-29 14:12:41 -0600

    Use plusSeconds to simplify adding seconds

--- kotlin/gigasecond/src/main/kotlin/Gigasecond.kt
+++ kotlin/gigasecond/src/main/kotlin/Gigasecond.kt
@@ -1,13 +1,8 @@
 import java.time.LocalDate
 import java.time.LocalDateTime
-import java.time.ZoneOffset
 
 class Gigasecond(date: LocalDateTime) {
-    val date: LocalDateTime =
-            LocalDateTime.ofInstant(
-                    date.toInstant(ZoneOffset.UTC).plusMillis(1_000_000_000_000),
-                    ZoneOffset.UTC.normalized()
-            )
+    val date: LocalDateTime = date.plusSeconds(1_000_000_000)
 
-    constructor(date: LocalDate) : this(date.atStartOfDay()) {}
+    constructor(date: LocalDate) : this(date.atStartOfDay())
 }
